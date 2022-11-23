d45c151b11a294eada6188060751842eff2c1dd3
Martin Balas <martin.balas@gmail.com>
2022-11-23 17:36:18 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ APP_ID="\"app_id\""
 
 for FILE in ./*.zat;
 do
+    cp $FILE .zat;
     if  grep -q "$APP_ID" "$FILE" ; then
         echo "App ID detected, running update" ; 
         zat clean
@@ -19,4 +20,5 @@ do
     else
         echo "Can't deploy. Please check your .zat file" ; 
     fi
+    rm -f .zat
 done
